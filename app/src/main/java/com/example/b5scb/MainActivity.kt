fcc9c6cacb98966d8a20f8047ce00c18ed99e49e
abdul <abdul.tanveer@gmail.com>
2024-01-08 12:05:00 +0530

    starting activities of other apps adn passing data to it

--- app/src/main/java/com/example/b5scb/MainActivity.kt
+++ app/src/main/java/com/example/b5scb/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.b5scb
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -33,5 +35,27 @@ class MainActivity : AppCompatActivity() {
         var homeIntent:Intent = Intent(this,HomeActivity::class.java)
         homeIntent.putExtra("nkey",data)
         startActivity(homeIntent)
+    }
+
+    fun openDialer(view: View) {
+        var dIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:9876512345"))
+        startActivity(dIntent)
+    }
+
+
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+      //  if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+       // }
+    }
+
+    fun setAlarm(view: View) {
+        createAlarm("b5 anndroid",12,4)
     }
 }

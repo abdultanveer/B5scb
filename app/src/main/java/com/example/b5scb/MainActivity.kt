f6d5a65575edf5034d08b76430faa1df1037d561
abdul <abdul.tanveer@gmail.com>
2024-01-08 14:25:05 +0530

    lifecycle activity- log-cat

--- app/src/main/java/com/example/b5scb/MainActivity.kt
+++ app/src/main/java/com/example/b5scb/MainActivity.kt
@@ -5,23 +5,70 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+//compat -- backwards compatibility
 class MainActivity : AppCompatActivity() {
     //EditText nameEt;
     lateinit var nameEt:EditText
     lateinit var submitBtn:Button
     lateinit var statusTv:TextView
+
+
+   // var TAG = "MainActivity"
+    var TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         nameEt = findViewById(R.id.etName) //taking handle
         submitBtn = findViewById(R.id.btnSubmmit)
         statusTv = findViewById(R.id.tvStatus)
+        Log.i(TAG,"activity is created")
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"activity is started")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w(TAG,"activity is resumed")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"activity is paused")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(TAG,"activity is stopped")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(TAG,"activity is restarted")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,"activity is destroyed")
+
+    }
+
+
+
 
     fun handleClick(mview: View) {
 

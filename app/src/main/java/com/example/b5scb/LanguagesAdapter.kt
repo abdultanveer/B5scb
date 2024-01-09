282a20fb6cfc7bc86b6c173c0f855d6e5fc3c056
abdul <abdul.tanveer@gmail.com>
2024-01-09 09:10:58 +0530

    recyclerview -1b

--- app/src/main/java/com/example/b5scb/LanguagesAdapter.kt
+++ app/src/main/java/com/example/b5scb/LanguagesAdapter.kt
@@ -1,6 +1,24 @@
 package com.example.b5scb
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 class LanguagesAdapter:RecyclerView.Adapter<LangViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LangViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: LangViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class LangViewHolder(row_layout_plank : View): ViewHolder(row_layout_plank) {
+
 }

897c8e3e9f1dee489c8d47011a793320610c12c6
abdul <abdul.tanveer@gmail.com>
2024-01-09 09:24:09 +0530

    recyclerview -adapter

--- app/src/main/java/com/example/b5scb/LanguagesAdapter.kt
+++ app/src/main/java/com/example/b5scb/LanguagesAdapter.kt
@@ -1,24 +1,37 @@
 package com.example.b5scb
 
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class LanguagesAdapter:RecyclerView.Adapter<LangViewHolder>() {
+class LanguagesAdapter(var languages: Array<String>) :RecyclerView.Adapter<LangViewHolder>() {
+
+    var TAG = LanguagesAdapter::class.java.simpleName
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LangViewHolder {
-        TODO("Not yet implemented")
+        Log.i(TAG,"ganesan bought a row plank from the market  and gave it to aravind")
+        var row_plank:View = LayoutInflater.from(parent.context).inflate(R.layout.row_layout_plank,parent,false)
+        return LangViewHolder(row_plank)
     }
 
     override fun onBindViewHolder(holder: LangViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        Log.i(TAG,"avishi is writing")
+        holder.tvRowItem.text = languages[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        Log.i(TAG,"arul counted items in dataset")
+        return  languages.size
     }
 }
 
 class LangViewHolder(row_layout_plank : View): ViewHolder(row_layout_plank) {
+    var tvRowItem = row_layout_plank.findViewById<TextView>(R.id.tvHeading)
+
 
 }
--- app/src/main/java/com/example/b5scb/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/b5scb/RecyclerViewActivity.kt
@@ -2,6 +2,8 @@ package com.example.b5scb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewActivity : AppCompatActivity() {
 
@@ -9,5 +11,10 @@ class RecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+
+        var adapter = LanguagesAdapter(languages)
+        var recyclerView:RecyclerView =  findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
     }
 }
